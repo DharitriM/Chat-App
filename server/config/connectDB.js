3e826bd063aead95fb2d515a77f06bf51809f316
DharitriM <dharitri.m.2001@gmail.com>
2024-09-13 19:36:19 +0530

    first commit

---
+++ server/config/connectDB.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGOOSE_URI);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("Connected to DB");
+    });
+    connection.on("error", (error) => {
+      console.log("Something is wrong in mongodb ", error);
+    });
+  } catch (error) {
+    console.log("something is wrong ", error);
+  }
+}
+
+module.exports = connectDB
